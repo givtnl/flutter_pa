6b700e8e24fc9ad83a31c0b2c4a12f6618151473
Michiel46 <michielverkest@gmail.com>
2021-08-23 10:24:58 +0200

    put the pull in the build script instead of in our script

--- generate-api-models.sh
+++ generate-api-models.sh
@@ -2,8 +2,6 @@
 
 rm -r ./lib/generated/api
 curl -o swagger-codegen-input.json 'https://0v9xof580f.execute-api.eu-west-3.amazonaws.com/prod/swagger/v1/swagger.json'
-launchctl start docker
-docker pull openapitools/openapi-generator-cli:latest
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli:latest generate -i /local/swagger-codegen-input.json -g dart -c /local/swagger-cli-config.json -o /local/lib/generated/api
 find ./lib/generated/api/lib -type f -name '*.dart' -exec sed -i '' s/OneOfo/O/ {} +
 find ./lib/generated/api/lib -type f -name '*.dart' -exec sed -i '' s/OneOf// {} +
