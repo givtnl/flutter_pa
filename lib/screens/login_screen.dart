c87ae847b1f0449e8eb2c503cc457b21164eae75
Mike Pattyn <mike@givtapp.net>
2021-08-24 11:51:36 +0200

    Dont need a stateful widget

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,59 +5,40 @@ import 'package:flutter_app/screens/intro_screen.dart';
 import 'package:flutter_app/widgets/blue_button.dart';
 import 'package:provider/provider.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   static const String routeName = '/login';
-
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final myTextFieldController = TextEditingController();
-  @override
-  void dispose() {
-    myTextFieldController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     var userProvider = Provider.of<UserProvider>(context);
     return Scaffold(
-      backgroundColor: Theme
-          .of(context)
-          .backgroundColor,
+      backgroundColor: Theme.of(context).backgroundColor,
       body: SafeArea(
           child: Stack(children: [
-            Center(
-              child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 50),
-                  child: TextField(
-                      controller: myTextFieldController,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Enter username or email'
-                      ),
-                      onChanged: (text) {
-                        userProvider.userName = text;
-                      }
-                  )
-              ),
+        Center(
+          child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter username or email'),
+                  onChanged: (text) {
+                    userProvider.userName = text;
+                  })),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: BlueButton(
+              label: "Start",
+              tapped: () {
+                Navigator.of(context).pushNamed(IntroScreen.routeName);
+              },
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: const EdgeInsets.all(50.0),
-                child: BlueButton(
-                  label: "Start",
-                  tapped: () {
-                    Navigator.of(context).pushNamed(IntroScreen.routeName);
-                  },
-                ),
-              ),
-            )
-          ])
-      ),
+          ),
+        )
+      ])),
     );
   }
 }
