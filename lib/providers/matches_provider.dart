7228c3e2c0585cf0a10c3261335d5ff23d4e9c58
Mike Pattyn <mike@givtapp.net>
2021-09-14 15:38:23 +0200

    fix bug

--- lib/providers/matches_provider.dart
+++ lib/providers/matches_provider.dart
@@ -5,19 +5,8 @@ class MatchesProvider with ChangeNotifier {
   late MatchesApi matchesApi;
 
   List<UserOrganisationMatchListModel> organisationMatches = [];
-  List<UserTagMatchListModel> tagScores = [];
 
-  late UserOrganisationMatchListModel selectedOrganisationMatch = UserOrganisationMatchListModel(
-      score: 90,
-      organisation: OrganisationDetailModel(
-          id: '1234',
-          name: 'Steun mij!',
-          description: 'lrem asd fjpaos djfasjd asdlfj laj pohsf pu i hpas d uof ih oiu oiuhiuho oiuho',
-          mission: 'io iuhoiuho ih oiuh ou uf oy ouyg ouoguygo yg',
-          vision: 'adgpp poihpoihp w a a sedfasdgf asd',
-          websiteUrl: 'www.steunerdesteun.be',
-          metaTags: {'sectors': 'animals',})
-  );
+  late UserOrganisationMatchListModel selectedOrganisationMatch;
 
   MatchesProvider() {
     this.matchesApi = MatchesApi();
@@ -29,9 +18,8 @@ class MatchesProvider with ChangeNotifier {
 
   Future<void> loadMatches(String userId) async {
     var response = await this.matchesApi.getMatchesList(userId: userId);
-    organisationMatches = response.organisationMatches;
-    tagScores = response.userTagMatches;
-    organisationMatches.sort((a, b) => b.score - a.score);
+    organisationMatches = response.result.toList();
+    organisationMatches.sort((a, b) => b.score.toInt() - a.score.toInt());
     notifyListeners();
   }
 
