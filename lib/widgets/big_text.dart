bc2d3fa4249a354450a82da17440665c6d50ea29
Michiel46 <michielverkest@gmail.com>
2021-09-15 17:10:05 +0200

    Meer text autosizen

--- lib/widgets/big_text.dart
+++ lib/widgets/big_text.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/themes/light/theme.dart';
 
@@ -8,9 +9,10 @@ class BigText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
+    return AutoSizeText(
         text,
         style: Theme.of(context).textTheme.headline1,
+        wrapWords: false,
       );
   }
 }
