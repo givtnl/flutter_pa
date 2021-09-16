1c252515865c5f3fd2792c3192e77cd1f4840cd4
Mike Pattyn <mike@givtapp.net>
2021-09-16 09:42:50 +0200

    remove logging

--- lib/widgets/background_patterns/MediaQueryService.dart
+++ lib/widgets/background_patterns/MediaQueryService.dart
@@ -15,25 +15,21 @@ class MediaQueryService {
 
   double getPercentageHeight(int percentage, String elementName) {
     var value = actualHeight * (percentage / 100);
-    print("${elementName} position Y: ${value}");
     return value;
   }
 
   double getPercentageWidth(int percentage, String elementName) {
     var value = actualWidth * (percentage / 100);
-    print("${elementName} position X: ${value}");
     return value;
   }
 
   double get actualHeight {
     var value = _screenSize.height;
-    print("Screen height: " + value.toString());
     return value;
   }
 
   double get actualWidth {
     var value = _screenSize.width;
-    print("Screen width: " + value.toString());
     return value;
   }
 
