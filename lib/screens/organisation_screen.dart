b451e2c0f6f79fb57a65a59d006037a452d9e8ac
Maarten Vergouwe <maarten@givtapp.net>
2021-10-26 15:21:34 +0200

    [EOY-253] Modulus on color selector

--- lib/screens/organisation_screen.dart
+++ lib/screens/organisation_screen.dart
@@ -47,14 +47,13 @@ class OrganisationScreen extends StatelessWidget {
 
     final Widget backArrow = SvgPicture.asset('assets/svg/back-arrow.svg');
 
-    final _tags = currentMatch.organisation.metaTags["sectors"]!
-        .split(",")
-        .map((e) => itlProvider.getSector(e));
+    final _tags = currentMatch.organisation.metaTags["sectors"]!.split(",").map((e) => itlProvider.getSector(e));
 
     final Widget fab = FloatingActionButton.large(
       onPressed: () async {
-        MixpanelManager.mixpanel.track("CLICKED",
-            properties: {"BUTTON_NAME": "SUPPORT_ORGANISATION"});
+        MixpanelManager.mixpanel.track("CLICKED", properties: {
+          "BUTTON_NAME": "SUPPORT_ORGANISATION"
+        });
         var url = currentOrganisation.metaTags["donationUrl"]!;
         if (await canLaunch(url))
           await launch(url);
@@ -91,9 +90,7 @@ class OrganisationScreen extends StatelessWidget {
                 padding: const EdgeInsets.only(bottom: 50),
                 child: Center(
                   child: Container(
-                    width: kIsWeb && MediaQuery.of(context).size.width > 700
-                        ? 700
-                        : double.infinity,
+                    width: kIsWeb && MediaQuery.of(context).size.width > 700 ? 700 : double.infinity,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -105,8 +102,7 @@ class OrganisationScreen extends StatelessWidget {
                               Navigator.of(context).pop();
                             },
                             iconSize: 15,
-                            splashRadius:
-                                .1, // put this on 25 because default (35) overlaps the app bar
+                            splashRadius: .1, // put this on 25 because default (35) overlaps the app bar
                           ),
                         ),
                         SizedBox(
@@ -123,16 +119,10 @@ class OrganisationScreen extends StatelessWidget {
                                   flex: 2,
                                   child: Align(
                                     alignment: Alignment.topLeft,
-                                    child: AutoSizeText(
-                                        currentOrganisation.name,
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .headline1!
-                                            .copyWith(fontSize: 24)),
+                                    child: AutoSizeText(currentOrganisation.name, style: Theme.of(context).textTheme.headline1!.copyWith(fontSize: 24)),
                                   ),
                                 ),
-                                MatchPercentageCircle(
-                                    currentMatch.score.round()),
+                                MatchPercentageCircle(currentMatch.score.round()),
                               ],
                             ),
                           ),
@@ -172,18 +162,11 @@ class OrganisationScreen extends StatelessWidget {
                               ),
                               Align(
                                 alignment: Alignment.centerLeft,
-                                child: AccentRoundedButton(
-                                    S
-                                        .of(context)
-                                        .organisationDetailScreen_visitWebsite,
-                                    () async {
-                                  if (await canLaunch(
-                                      currentOrganisation.websiteUrl)) {
-                                    await launch(
-                                        currentOrganisation.websiteUrl);
+                                child: AccentRoundedButton(S.of(context).organisationDetailScreen_visitWebsite, () async {
+                                  if (await canLaunch(currentOrganisation.websiteUrl)) {
+                                    await launch(currentOrganisation.websiteUrl);
                                   } else {
-                                    Navigator.of(context)
-                                        .pushNamed(ErrorScreen.routeName);
+                                    Navigator.of(context).pushNamed(ErrorScreen.routeName);
                                   }
                                 }, Theme.of(context).buttonColor, true),
                               ),
@@ -196,21 +179,15 @@ class OrganisationScreen extends StatelessWidget {
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     Text(
-                                      S
-                                          .of(context)
-                                          .organisationDetailScreen_youMatchTitle,
-                                      style:
-                                          Theme.of(context).textTheme.headline2,
+                                      S.of(context).organisationDetailScreen_youMatchTitle,
+                                      style: Theme.of(context).textTheme.headline2,
                                     ),
                                     SizedBox(
                                       height: 10,
                                     ),
                                     Text(
-                                      S
-                                          .of(context)
-                                          .organisationDetailScreen_youMatchSubTitle,
-                                      style:
-                                          Theme.of(context).textTheme.subtitle1,
+                                      S.of(context).organisationDetailScreen_youMatchSubTitle,
+                                      style: Theme.of(context).textTheme.subtitle1,
                                     ),
                                   ],
                                 ),
@@ -223,18 +200,12 @@ class OrganisationScreen extends StatelessWidget {
                                   physics: NeverScrollableScrollPhysics(),
                                   itemBuilder: (ctx, idx) {
                                     return Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
                                       children: [
                                         Text(
-                                          currentTags
-                                              .elementAt(idx)
-                                              .tag
-                                              .toUpperCase(),
+                                          currentTags.elementAt(idx).tag.toUpperCase(),
                                           textAlign: TextAlign.left,
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .bodyText2,
+                                          style: Theme.of(context).textTheme.bodyText2,
                                         ),
                                         SizedBox(
                                           height: 5,
@@ -245,29 +216,16 @@ class OrganisationScreen extends StatelessWidget {
                                             Icon(
                                               Givt.user_icon,
                                               size: 20,
-                                              color: Theme.of(context)
-                                                  .primaryColor,
-                                              semanticLabel:
-                                                  "Jouw score op tag " +
-                                                      currentOrganisationTags[
-                                                              idx]
-                                                          .tag,
+                                              color: Theme.of(context).primaryColor,
+                                              semanticLabel: "Jouw score op tag " + currentOrganisationTags[idx].tag,
                                             ),
                                             SizedBox(width: 10),
                                             CustomLinearProgressIndicator(
                                               height: 20,
                                               borderRadius: 20,
-                                              value: currentTags
-                                                      .elementAt(idx)
-                                                      .score
-                                                      .toDouble() /
-                                                  100,
-                                              color: getColorForIndicator(
-                                                  idx, context),
-                                              backgroundColor:
-                                                  getColorForIndicator(
-                                                          idx, context)
-                                                      .withOpacity(0.15),
+                                              value: currentTags.elementAt(idx).score.toDouble() / 100,
+                                              color: getColorForIndicator(idx, context),
+                                              backgroundColor: getColorForIndicator(idx, context).withOpacity(0.15),
                                             ),
                                           ],
                                         ),
@@ -278,62 +236,42 @@ class OrganisationScreen extends StatelessWidget {
                                             Icon(
                                               Givt.org_icon,
                                               size: 20,
-                                              color: Theme.of(context)
-                                                  .primaryColor,
-                                              semanticLabel:
-                                                  "Score organisatie " +
-                                                      currentOrganisationTags[
-                                                              idx]
-                                                          .tag,
+                                              color: Theme.of(context).primaryColor,
+                                              semanticLabel: "Score organisatie " + currentOrganisationTags[idx].tag,
                                             ),
                                             SizedBox(width: 10),
                                             CustomLinearProgressIndicator(
                                               height: 20,
                                               borderRadius: 20,
-                                              value: provider
-                                                      .getOrganisationTagScore(
-                                                          currentOrganisationTags[
-                                                                  idx]
-                                                              .tag)
-                                                      .toDouble() /
-                                                  100,
-                                              color: getColorForIndicator(
-                                                  idx, context),
-                                              backgroundColor:
-                                                  getColorForIndicator(
-                                                          idx, context)
-                                                      .withOpacity(0.15),
+                                              value: provider.getOrganisationTagScore(currentOrganisationTags[idx].tag).toDouble() / 100,
+                                              color: getColorForIndicator(idx, context),
+                                              backgroundColor: getColorForIndicator(idx, context).withOpacity(0.15),
                                             ),
                                           ],
                                         )
                                       ],
                                     );
                                   },
-                                  separatorBuilder:
-                                      (BuildContext context, int index) {
+                                  separatorBuilder: (BuildContext context, int index) {
                                     return SizedBox(
                                       height: 20,
                                     );
                                   },
                                   itemCount: currentTags.length),
-                              if (currentOrganisation.metaTags
-                                      .containsKey("donationUrl") &&
-                                  MediaQuery.of(context).size.height >= 900)
+                              if (currentOrganisation.metaTags.containsKey("donationUrl") && MediaQuery.of(context).size.height >= 900)
                                 Padding(
                                   padding: const EdgeInsets.only(top: 30.0),
                                   child: MainButton(
-                                    label: S
-                                        .of(context)
-                                        .organisationDetailScreen_giveButton,
+                                    label: S.of(context).organisationDetailScreen_giveButton,
                                     tapped: () async {
-                                      MixpanelManager.mixpanel
-                                          .track("CLICKED", properties: {
+                                      MixpanelManager.mixpanel.track("CLICKED", properties: {
                                         "BUTTON_NAME": "SUPPORT_ORGANISATION"
                                       });
-                                      var url = currentOrganisation
-                                          .metaTags["donationUrl"]!;
+                                      var url = currentOrganisation.metaTags["donationUrl"]!;
                                       if (kIsWeb) {
-                                        js.context.callMethod('open', [url]);
+                                        js.context.callMethod('open', [
+                                          url
+                                        ]);
                                       } else {
                                         if (await canLaunch(url))
                                           await launch(url);
@@ -356,20 +294,18 @@ class OrganisationScreen extends StatelessWidget {
             ),
           ]),
         ),
-        floatingActionButton:
-            (currentOrganisation.metaTags.containsKey("donationUrl") &&
-                    MediaQuery.of(context).size.height < 900)
-                ? Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Container(
-                      height: 65,
-                      width: 65,
-                      child: FittedBox(
-                        child: fab,
-                      ),
-                    ),
-                  )
-                : null,
+        floatingActionButton: (currentOrganisation.metaTags.containsKey("donationUrl") && MediaQuery.of(context).size.height < 900)
+            ? Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  height: 65,
+                  width: 65,
+                  child: FittedBox(
+                    child: fab,
+                  ),
+                ),
+              )
+            : null,
         floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       ),
     );
@@ -378,7 +314,7 @@ class OrganisationScreen extends StatelessWidget {
 
 Color getColorForIndicator(int idx, BuildContext context) {
   int number = idx;
-  switch (number) {
+  switch (number % 3) {
     case 0:
       return Theme.of(context).accentColor;
     case 1:
@@ -389,4 +325,3 @@ Color getColorForIndicator(int idx, BuildContext context) {
       return Theme.of(context).primaryColor;
   }
 }
-
